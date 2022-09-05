e833a340b61f4270aa77c3f934108f0b9e3be0a3
Pedro Morais <pmor4is@gmail.com>
2022-09-05 19:46:40 -0300

    moldando elementos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_personal_expenses/models/transaction.dart';
+
+main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({Key? key}) : super(key: key);
@@ -8,8 +11,47 @@ class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: MyHomePage(),
     );
-    
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'tenis',
+      value: 310.89,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'camisa',
+      value: 41.10,
+      date: DateTime.now(),
+    )
+  ];
+
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(
+            child: Text('a'),
+          ),
+          Card(
+            child: Text('a'),
+          ),
+        ],
+      ),
+    );
   }
 }
